149cf366ccafca46c57bc8d2938802a166e2a10b
andreev-oe <andreev.oe@yandex.ru>
2022-07-31 19:33:58 +0300

    fix: solve problems opening/closing for upload form

--- js/modules/form/index.js
+++ js/modules/form/index.js
@@ -1,17 +1,40 @@
 const formUploadElement = document.querySelector('#upload-file');
 const formUploadOverlayElement = document.querySelector('.img-upload__overlay');
-const body = document.querySelector('body');
+const bodyElement = document.querySelector('body');
 const uploadCancelElement = document.querySelector('#upload-cancel');
+const imageUploadWrapperElement = document.querySelector('.img-upload__wrapper');
+const formInputElements = imageUploadWrapperElement.querySelectorAll('input, textarea');
+const scaleControlValue = imageUploadWrapperElement.querySelector('.scale__control--value');
+
+const clearUploadForm = () => {
+  for (const field of formInputElements) {
+    field.checked = field.value === 'none';
+    if (field.type !== 'radio') {
+      field.value = '';
+    }
+  }
+  scaleControlValue.value = '55%';
+};
 
 const onUploadFormChange = () => {
-  body.classList.add('modal-open');
+  bodyElement.classList.add('modal-open');
   formUploadOverlayElement.classList.remove('hidden');
 };
 
 const onUploadFormCancelClick = () => {
+  bodyElement.classList.remove('modal-open');
   formUploadOverlayElement.classList.add('hidden');
-  body.classList.remove('modal-open');
+  clearUploadForm();
 };
 
+const onBodyEscapeKeydown = (evtKey) => {
+  if (evtKey.key === 'Escape') {
+    bodyElement.classList.remove('modal-open');
+    formUploadOverlayElement.classList.add('hidden');
+    clearUploadForm();
+  }
+};
+
+bodyElement.addEventListener('keydown', onBodyEscapeKeydown);
 formUploadElement.addEventListener('change', onUploadFormChange);
 uploadCancelElement.addEventListener('click', onUploadFormCancelClick);
